56aa018df75caca6cf7a1d0512fcc9e08668d894
LuckyTW <skqkstjr2@gmail.com>
2021-07-20 00:08:10 +0900

    data_structure_1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,372 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delievered!`
+    );
+  },
+
+  orderPaster: function (ing1, ing2, ing3) {
+    console.log(`here is ur delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sola, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// //default values
+// const { menu = [], starterMenu: starter = [] } = restaurant;
+// console.log(menu, starter);
+
+// //mutating variables
+// let a = 111;
+// let b = 999;
+
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// //nested objects
+
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+// // Spread Operators
+
+// const arr = [7, 8, 9];
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+// console.log(...newArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// //Join 2 arrays
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// //Iterables: arr, strings, maps, sets, Not objects
+
+// const str = 'Jonas';
+// const letters = [...str, ' ', 'S.'];
+// console.log(letters);
+
+// // const ingredients = [
+// //   prompt(`Let's make pasta ingredients 1?`),
+// //   prompt(`Let's make pasta ingredients 2?`),
+// //   prompt(`Let's make pasta ingredients 3?`),
+// // ];
+// // console.log(ingredients);
+
+// // restaurant.orderPaster(...ingredients);
+
+// //objects
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Rome';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+// //Rest Parameter
+// // 스프레드와 반대. 스프레드가 unpack를 하는 반면, REST는 array화 시켜주는 역할
+// // 스프레드는 =는 오른쪽, 레스트는 왼쪽에 위치
+
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// //rest는 중간에 생략된 내용들을 포함하지 않음!
+// //그래서 항상 마지막에 써줘야함
+
+// //Objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+// //functions
+// const add = function (...numbers) {
+//   console.log(numbers);
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     //pack the values into arrs
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(6, 4, 3, 2, 3, 6, 8);
+
+// const x = [23, 5, 7];
+// add(...x); // unpack the values
+
+// Short Circuiting
+
+// console.log(3 || 'jonas'); // 만약 처음 값이 truthy라면 다음 값에 상관없이 처음값을 리턴
+// console.log('' || 'jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+// restaurant.numGuests = 0;
+// const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guest1);
+
+// const guest2 = restaurant.numGuests || 10; // short-circuiting 을 사용하는 방법!
+// console.log(guest2);
+
+// console.log('--------And--------');
+
+// console.log(0 && 'Jonas'); // 첫 값이 falsy라면 다음 값에 상관없이 처음 값을 리턴!모두 truthy 일경우 마지막 값 리턴
+// console.log(7 && 'Jonas');
+
+// console.log('hello' && 23 && null && 'jonas');
+
+// //nullish: null and undefined(Not 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+///////////////////////////////////////////////////////
+
+// Coding Challenge #1
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+// //1
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+// //2
+// const [goalkeeper, ...fieldPlayers] = players1;
+// console.log(goalkeeper);
+
+// //3
+// const allPlayers = [...players1, ...players2];
+
+// //4
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// //5
+// // const { team1, x: draw, team2 } = game.odds;
+// // 다른 방법
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+
+// console.log(team1, draw, team2);
+
+// //6
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored!`);
+// };
+
+// printGoals('Davies', 'Muller');
+// printGoals(...game.scored);
+
+// //7
+// team1 < team2 && console.log('Team 1 is more likely to win!');
+
+// //optional chaining
+
+// console.log(restaurant.openingHours.mon?.open); //-> restaurant.openingHours.mon 존재하는 경우에만 .open 실행
+// // console.log(restaurant.openingHours.mon.open);
+
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (let day of days) {
+//   restaurant.openingHours[day]; // 만약 variable(day)를 쓰려면 bracket notation를 사용해야함! .day로 못씀
+//   const open = restaurant.openingHours[day]?.open ?? 'closed'; // new concept of nullish
+//   console.log(`on ${day}. we open at ${open}`);
+// }
+
+// //Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// //Arrays
+// // const users = [{ name: 'Jonas', email: 'skqkstjr2@gmail.com' }];
+// const users = [];
+
+// console.log(users[0]?.name ?? 'User array empty!');
+
+// // Looping Objects
+// // Property Names
+// const properties = Object.keys(restaurant.openingHours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days: `;
+
+// for (let day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// //Property Values
+// const values = Object.values(restaurant.openingHours);
+// console.log(values);
+
+// //Entire Obj
+// const entries = Object.entries(restaurant.openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   // destructuring! 참고할 것
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+//1.
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+//2.
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (let el of odds) average += el;
+average /= odds.length;
+console.log(average);
+
+//3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  // 배열의 경우는 entries뒤에 ()만 넣어줌
+  // console.log(team, odd);
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
