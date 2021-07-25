ff64643c656dd02d1d481ab414ed5f0c419000bb
LuckyTW <skqkstjr2@gmail.com>
2021-07-25 14:17:59 +0900

    more on functions1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,81 @@
 'use strict';
+// // default parameters
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassenger = numPassenger || 1;
+
+//   const booking = { flightNum, numPassengers, price };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+
+// //How passing Arguments Works:value vs Reference
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 21345145,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 21345145) {
+//     alert('Checked In!');
+//   } else {
+//     alert('Wrong Passport!');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order func
+const transformer = function (str, fn) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+// JS uses callbacks all the time
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+const high5 = () => {
+  console.log('play!');
+};
+
+document.body.addEventListener('click', high5);
