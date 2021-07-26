de67ee3595c5906558e6dfcd8bfe41e6d8fbc1f2
LuckyTW <skqkstjr2@gmail.com>
2021-07-26 16:59:12 +0900

    coding challenge

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -80,118 +80,173 @@
 
 // document.body.addEventListener('click', high5);
 
-//functions returning functions
+// //functions returning functions
 
-const greet = greeting => {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-};
+// const greet = greeting => {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
 
-const greeterHey = greet('hey');
-greeterHey('joans');
-greeterHey('steven');
+// const greeterHey = greet('hey');
+// greeterHey('joans');
+// greeterHey('steven');
 
-greet('hello')('Jonas');
+// greet('hello')('Jonas');
 
-//call and apply Methods
+// //call and apply Methods
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({
+//       flight: `${this.airline} flight ${this.iataCode}${flightNum}`,
+//       name,
+//     });
+//   },
+// };
+
+// lufthansa.book(238, 'Lucky Na');
+// lufthansa.book(635, 'Jogn Smith');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// const book = lufthansa.book;
+
+// // does not work
+// //book(23, 'Sarah Williams')
+
+// book.call(eurowings, 23, 'Sarah Williams'); // call 메소드는 첫번째 인자에 'this'키워드를 설정함. 다양한 객체에 접근가능
+// console.log(eurowings);
+
+// book.call(lufthansa, 239, 'Mary Cooper');
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Line',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+
+// book.call(swiss, 52, 'Lucky TW');
+// console.log(swiss);
+
+// //Apply method -> call와 완전똑같지만, 뒤에 인자값을 받는것이 아니라 배열일 받음!
+// const flightData = [583, 'George'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+
+// //하지만 보통 call+ spread operator를 많이씀
+// book.call(swiss, ...flightData);
+
+// // bind Method
+// // 앞에서 call& apply와 비슷하게 manually set the 'this' keyword in the function call
+// // return new function where this keyword is bound
+
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, 'Steven Williams'); // 이미 this가 고정적으로 eurowings를 향하고있음
+
+// const bookEW23 = book.bind(eurowings, 23); // flightNum을 미리 preset 해줄 수도 있음 -> partial application
+// bookEW23('Jonas Schmedtmann');
+// bookEW23('Martha Cooper');
+
+// //Event Listeners
+// lufthansa.plane = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.plane++;
+//   console.log(this.plane);
+// };
+
+// // lufthansa.buyPlane();
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // 이경우 this는 button element를 가리키게 됨 -> bind를 사용해서 새로운 함수를 리턴해야함
+
+// //partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // 위의 것과 똑같은 의미로 addVAT = value => value + value * 0.23;
+
+// console.log(addVAT(100));
+
+// // challenge -> closure 함수로 만들어보기
+// const addTaxRate = rate => {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+
+//coding challenge
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    //Get Answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
     );
-    this.bookings.push({
-      flight: `${this.airline} flight ${this.iataCode}${flightNum}`,
-      name,
-    });
+    // console.log(answer);
+
+    //Register
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
-
-lufthansa.book(238, 'Lucky Na');
-lufthansa.book(635, 'Jogn Smith');
-console.log(lufthansa);
-
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-
-const book = lufthansa.book;
-
-// does not work
-//book(23, 'Sarah Williams')
-
-book.call(eurowings, 23, 'Sarah Williams'); // call 메소드는 첫번째 인자에 'this'키워드를 설정함. 다양한 객체에 접근가능
-console.log(eurowings);
-
-book.call(lufthansa, 239, 'Mary Cooper');
-console.log(lufthansa);
-
-const swiss = {
-  airline: 'Swiss Air Line',
-  iataCode: 'LX',
-  bookings: [],
-};
-
-book.call(swiss, 52, 'Lucky TW');
-console.log(swiss);
-
-//Apply method -> call와 완전똑같지만, 뒤에 인자값을 받는것이 아니라 배열일 받음!
-const flightData = [583, 'George'];
-book.apply(swiss, flightData);
-console.log(swiss);
-
-//하지만 보통 call+ spread operator를 많이씀
-book.call(swiss, ...flightData);
-
-// bind Method
-// 앞에서 call& apply와 비슷하게 manually set the 'this' keyword in the function call
-// return new function where this keyword is bound
-
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-
-bookEW(23, 'Steven Williams'); // 이미 this가 고정적으로 eurowings를 향하고있음
-
-const bookEW23 = book.bind(eurowings, 23); // flightNum을 미리 preset 해줄 수도 있음 -> partial application
-bookEW23('Jonas Schmedtmann');
-bookEW23('Martha Cooper');
-
-//Event Listeners
-lufthansa.plane = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.plane++;
-  console.log(this.plane);
-};
-
-// lufthansa.buyPlane();
+// poll.registerNewAnswer();
 
 document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // 이경우 this는 button element를 가리키게 됨 -> bind를 사용해서 새로운 함수를 리턴해야함
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-//partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 
-const addVAT = addTax.bind(null, 0.23);
-// 위의 것과 똑같은 의미로 addVAT = value => value + value * 0.23;
+// Immediately Invoked Function Expression(IIFE)
 
-console.log(addVAT(100));
-
-// challenge -> closure 함수로 만들어보기
-const addTaxRate = rate => {
-  return function (value) {
-    return value + value * rate;
-  };
+const runOnce = function () {
+  console.log('this will never run again');
 };
 
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
+runOnce();
+
+(function () {
+  console.log('this will never run again');
+})(); //요거 전체가 하나의 값을 의미!
+
+//화살표함수도 마찬가지
+(() => console.log('this will also never run again'))();
