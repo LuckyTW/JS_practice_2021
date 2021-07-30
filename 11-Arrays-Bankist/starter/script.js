b84b45ccfe1f5474aee7e8f407b4dc5ac6eff594
LuckyTW <skqkstjr2@gmail.com>
2021-07-30 13:43:56 +0900

    array map

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,77 +61,180 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const user = 'Steven Thomas Williams'; //stw
+const createUsernames = function (acc) {
+  acc.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  }); //기존의 객체에서 작업해주는 것이기 때문에 리턴하지 않아도 됨
+};
+
+createUsernames(accounts);
+
+// console.log(containerMovements.innerHTML);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-//SLICE
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
+// //SLICE
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice());
+// console.log([...arr]);
 
-//SPLICE
-// console.log(arr.splice(2));
-arr.splice(-1);
-arr.splice(1, 2); // position 1에서 2개를 삭제
-console.log(arr);
+// //SPLICE
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// arr.splice(1, 2); // position 1에서 2개를 삭제
+// console.log(arr);
 
-//REVERSE
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-// mutable
+// //REVERSE
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// // mutable
 
-//CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]); //선호의 차이
+// //CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]); //선호의 차이
 
-//JOIN
-console.log(letters.join(' - '));
+// //JOIN
+// console.log(letters.join(' - '));
 
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: you deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: you withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// //forEach => 멈출수 있는 방법이 없음. break 와 continue 사용 불가
+
+// console.log('-----------------------------');
+// movements.forEach(function (movement, i, array) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: you deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: you withdrew ${Math.abs(movement)}`);
+//   }
+// });
+
+// //forEach with Map and Set
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// const currenciesUniq = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUniq);
+// currenciesUniq.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`); // 어차피 uniq한 밸류라 key값이 의미가 없음. 그냥 요식행위
+// });
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+//   console.log(dogsJuliaCorrected);
+
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
+
+//   dogs.forEach(function (dog, i) {
+//     if (dog >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+//Map
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// for (const movement of movements) {
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: you deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: you withdrew ${Math.abs(movement)}`);
-  }
-}
+const eurToUsd = 1.1;
 
-//forEach => 멈출수 있는 방법이 없음. break 와 continue 사용 불가
+const movementUsd = movements.map(mov => mov * eurToUsd);
 
-console.log('-----------------------------');
-movements.forEach(function (movement, i, array) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: you deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: you withdrew ${Math.abs(movement)}`);
-  }
-});
+console.log(movementUsd);
 
-//forEach with Map and Set
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
 
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
+const movementDes = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: you ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
 
-const currenciesUniq = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUniq);
-currenciesUniq.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`); // 어차피 uniq한 밸류라 key값이 의미가 없음. 그냥 요식행위
-});
+console.log(movementDes);
