880205ec77bb129c6a2bc9c2136391c6b10a6102
LuckyTW <skqkstjr2@gmail.com>
2021-09-01 20:17:43 +0900

    ES6 classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,41 +1,117 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  // console.log(this);
-  //Instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   // console.log(this);
+//   //Instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  //Never do this
-  // this.calcAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
+//   //Never do this
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
+
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
+
+// const matilda = new Person('matilda', 2017);
+// const jack = new Person('jack', 1975);
+
+// console.log(matilda, jack);
+// console.log(jonas instanceof Person);
+
+// //1. new empty object is created
+// //2. function is called, this = {}
+// //3. {} linked to prototype
+// //4. function automatically return {}
+
+// //Prototypes
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// jonas.calcAge();
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype); //여기서 prototype은 Person의 프로토타입을 가리키는 것이 아니라 Person과 연결된 객체들의 프로토타입을 의미함 -> prototype of linked objects
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas, matilda);
+// console.log(jonas.hasOwnProperty('firstName'));
+
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+// const arr = [1, 2, 3, 5, 5, 3]; // new Array === []
+// console.log(arr.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
-
-const matilda = new Person('matilda', 2017);
-const jack = new Person('jack', 1975);
-
-console.log(matilda, jack);
-console.log(jonas instanceof Person);
-
-//1. new empty object is created
-//2. function is called, this = {}
-//3. {} linked to prototype
-//4. function automatically return {}
-
-//Prototypes
-
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-jonas.calcAge();
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype); //여기서 prototype은 Person의 프로토타입을 가리키는 것이 아니라 Person과 연결된 객체들의 프로토타입을 의미함 -> prototype of linked objects
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas, matilda);
-console.log(jonas.hasOwnProperty('firstName'));
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 85);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+
+//ES6
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+jessica.greet();
